9db30ccdf9a912e07608e39bf155867a0f9dc03b
Domingos Latorre <domingos.oliveira@ifsp.edu.br>
2022-04-08 12:18:38 -0300

    Refactor to using and var

--- UsuarioDao.cs
+++ UsuarioDao.cs
@@ -5,33 +5,33 @@ public class UsuarioDao : IUsuarioDao
 
     public UsuarioDao()
     {
-        SqliteConnection connection = new SqliteConnection("Data Source=database.db");
-        connection.Open();
-        
-        SqliteCommand command = connection.CreateCommand();
-        command.CommandText = 
-        @"
-        CREATE TABLE IF NOT EXISTS Usuario (
-            codigo int not null primary key,
-            nome varchar(100),
-            email varchar(100),
-            senha varchar(3),
-            ativo bool
-        );   
-        ";
-        command.ExecuteNonQuery();
-        connection.Close();
+        using(var connection = new SqliteConnection("Data Source=database.db"))
+        {
+            connection.Open();
+            
+            var command = connection.CreateCommand();
+            command.CommandText = 
+            @"
+            CREATE TABLE IF NOT EXISTS Usuario (
+                codigo int not null primary key,
+                nome varchar(100),
+                email varchar(100),
+                senha varchar(3),
+                ativo bool
+            );   
+            ";
+            command.ExecuteNonQuery();
+        }
     }
 
     public Usuario Save(Usuario usuario)
     {
-        SqliteConnection connection = new SqliteConnection("Data Source=database.db");
+        using var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
         
-        SqliteCommand command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = $"INSERT INTO Usuario VALUES({usuario.Codigo}, '{usuario.Nome}', '{usuario.Email}', '{usuario.Senha}', {usuario.Ativo})";
         command.ExecuteNonQuery();
-        connection.Close();
 
         return usuario;
     }
