ce4d4dbb97f448d284a13ce904b204df9fffa6a4
Domingos Latorre <domingos.latorre@gmail.com>
2022-04-14 16:53:26 -0300

    Repository and ef

--- Program.cs
+++ Program.cs
@@ -2,44 +2,45 @@
 
 var databaseSetup = new DatabaseSetup(databaseConfig);
 
-//IUsuarioRepository usuarioRepository = new UsuarioRepositoryDapper(databaseConfig);
-
-//Usuario joao = usuarioRepository.Save(new Usuario(10, "João", "joao@email.com", "123"));
-//Usuario maria = usuarioRepository.Save(new Usuario(11, "Maria", "maria@email.com", "abc", true));
-
-//Console.WriteLine(usuarioRepository.GetById(3));
-//Console.WriteLine("-------");
-
-//Usuario usuarioAtualizado = usuarioRepository.Update(new Usuario(8, "XXXXX", "xxxxx@email.com", "xxx", false));
-// Console.WriteLine(usuarioAtualizado);
-//usuarioRepository.GetAll().ToList().ForEach(u => Console.WriteLine(u));
-
-//Console.WriteLine("-------");
-//usuarioRepository.Delete(10);
-//usuarioRepository.GetAll().ToList().ForEach(u => Console.WriteLine(u));
-
-
 using var db = new SistemaContext(databaseConfig);
+
+IUsuarioRepository usuarioRepository = new UsuarioRepositoryEF(db);
+
+Usuario joao = usuarioRepository.Save(new Usuario(1, "João", "joao@email.com", "123"));
+Usuario maria = usuarioRepository.Save(new Usuario(2, "Maria", "maria@email.com", "abc", true));
+Usuario pedro = usuarioRepository.Save(new Usuario(3, "Pedro", "pedro@email.com", "abc", true));
+
+Console.WriteLine(usuarioRepository.GetById(2));
+Console.WriteLine("-------");
+
+// Usuario usuarioAtualizado = usuarioRepository.Update(new Usuario(8, "XXXXX", "xxxxx@email.com", "xxx", false));
+// Console.WriteLine(usuarioAtualizado);
+usuarioRepository.GetAll().ToList().ForEach(u => Console.WriteLine(u));
+
+Console.WriteLine("-------");
+usuarioRepository.Delete(2);
+usuarioRepository.GetAll().ToList().ForEach(u => Console.WriteLine(u));
+
 
 //db.Add(new Usuario(1, "João", "joao@email.com", "123"));
 //db.Add(new Usuario(2, "Maria", "maria@email.com", "abc", true));
 //db.SaveChanges();
 
-db.Usuarios.ToList().ForEach(u => Console.WriteLine(u));
+// db.Usuarios.ToList().ForEach(u => Console.WriteLine(u));
 
 // Unhandled exception. System.InvalidOperationException: The instance of entity type 'Usuario' 
 // cannot be tracked because another instance with the same key value for {'Id'} is already being tracked. 
-db.ChangeTracker.Clear();
+// db.ChangeTracker.Clear();
 
 
-db.Usuarios.Update(new Usuario(2, "Maria da Silva", "mariasilva@email.com", "abcdef", false));
-db.SaveChanges();
+// db.Usuarios.Update(new Usuario(2, "Maria da Silva", "mariasilva@email.com", "abcdef", false));
+// db.SaveChanges();
 
-Console.WriteLine(db.Usuarios.Single(u => u.Id == 2));
-var usuario17 = db.Usuarios.Find(17);
-Console.WriteLine(usuario17);
+// Console.WriteLine(db.Usuarios.Single(u => u.Id == 2));
+// var usuario17 = db.Usuarios.Find(17);
+// Console.WriteLine(usuario17);
 
-db.Usuarios.Remove(usuario17);
-db.SaveChanges();
+// db.Usuarios.Remove(usuario17);
+//db.SaveChanges();
 
-db.Usuarios.ToList().ForEach(u => Console.WriteLine(u));
+// db.Usuarios.ToList().ForEach(u => Console.WriteLine(u));
---
+++ UsuarioRepositoryEF.cs
@@ -0,0 +1,33 @@
+public class UsuarioRepositoryEF : IUsuarioRepository
+{
+    private SistemaContext sistemaContext;
+
+    public UsuarioRepositoryEF(SistemaContext sistemaContext)
+    {
+        this.sistemaContext = sistemaContext;    
+    }
+
+    public void Delete(int id)
+    {
+        sistemaContext.Usuarios.Remove(GetById(id));
+        sistemaContext.SaveChanges();
+    }
+
+    public IEnumerable<Usuario> GetAll() => sistemaContext.Usuarios.ToList();
+
+    public Usuario GetById(int id) => sistemaContext.Usuarios.Find(id);
+
+    public Usuario Save(Usuario usuario)
+    {
+        sistemaContext.Add(usuario);
+        sistemaContext.SaveChanges();
+        return usuario;
+    }
+
+    public Usuario Update(Usuario usuario)
+    {
+        sistemaContext.Update(usuario);
+        sistemaContext.SaveChanges();
+        return usuario;
+    }
+}
